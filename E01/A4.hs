2d15cc000bf4de74fb9af5f359686714c825e686
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2014-04-29 20:14:13 +0200

    Add solution for exercise 1, assignment 4

---
+++ E01/A4.hs
@@ -0,0 +1,20 @@
+module E01.A4 where
+
+-- (a)
+-- Liste aller positiven ganzen Zahlen s = [1,2,3,...]
+
+gen :: [Int]
+gen = gen1 1
+
+gen1 :: Int -> [Int]
+gen1 a = a:gen1 (a+1)
+
+-- (b)
+{-
+  Haskell nutzt lazy-evaluation oder auch call-by-need genannt, obwohl der
+  Begriff call im Kontext von Haskell mit Vorsicht zu genießen ist.
+  Dies erlaubt es Kontrukte wie in Aufgabe (a), eine Liste, die nie endet.
+  Würde eine call-by-value-Strategie angewendet, könnte ein Aufruf der
+  Funktion nie terminieren.
+-}
+
