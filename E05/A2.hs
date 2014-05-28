6b74d14ab00bf76d1bd5a9b18747e00942cd7862
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2014-05-28 14:09:13 +0200

    Add solution for exercise 5 assignment 2

---
+++ E05/A2.hs
@@ -0,0 +1,26 @@
+module A2 where
+
+data Tree a = Node a [Tree a] deriving Show
+
+class Zippable z where
+  genericZipWith :: (a -> b -> c) -> z a -> z b -> z c
+  genericZip :: z a -> z b -> z (a, b)
+  -- Hinweis: Der Operator (,) :: a -> b -> (a, b) erzeugt aus zwei Werten
+  -- beliebigen Typs ein Tupel
+  genericZip = genericZipWith (,)
+
+-- Die Funktionen zip und zipWith für Listen existieren ja bereits, also ist die
+-- die Definition für den Listentyp [] trivial.
+instance Zippable [] where
+  genericZipWith = zipWith
+  genericZip = zip
+
+-- Für den Tree wird es nun schon etwas haariger. 
+instance Zippable Tree where
+  -- Bedeutung der Variablennames:
+  -- - f function
+  -- - lv, rv: left bzw. right value
+  -- - lcs, rcs: left bzw. right children
+  genericZipWith f (Node lv lcs) (Node rv rcs)
+    = Node (f lv rv) (zipWith (genericZipWith f) lcs rcs)
+
